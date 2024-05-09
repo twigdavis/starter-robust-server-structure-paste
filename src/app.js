5c64f7ca563f3d5bdddcf1780dfdae4a3f4571d4
Justin Davis <judavis@tesla.com>
2024-05-09 10:56:28 -0400

    lots of work

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const pastes = require('./data/pastes-data');
+const pastesRouter = require("./pastes/pastes.router");
+const usersRouter = require("./users/users.router");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+app.use(express.json())
+
+app.use("/users", usersRouter)
+app.use("/pastes", pastesRouter);
 
 // Not found handler
 app.use((request, response, next) => {
@@ -9,9 +15,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
